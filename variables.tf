f1f89186b938a2b5f6e384772e990db57575c993
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2022-03-03 17:17:52 -0500

    Add the user creation configuration
    This configuration is sourced (with minor edits) from:
    https://github.com/cisagov/cool-users-non-admin/blob/develop/users.tf

--- variables.tf
+++ variables.tf
@@ -4,6 +4,11 @@
 # You must provide a value for each of these parameters.
 # ------------------------------------------------------------------------------
 
+variable "users" {
+  description = "A map whose keys are the usernames of each non-admin user and whose values are a map containing supported user attributes.  The only currently-supported attribute is \"require_mfa\" (boolean).  Example: { \"firstname1.lastname1\" = { \"require_mfa\" = false }, \"firstname2.lastname2\" = { \"require_mfa\" = true }, \"firstname3.lastname3\" = { \"require_mfa\" = false } }"
+  type        = map(map(string))
+}
+
 # ------------------------------------------------------------------------------
 # OPTIONAL PARAMETERS
 #
@@ -14,6 +19,12 @@ variable "aws_region" {
   default     = "us-east-1"
   description = "The AWS region where the non-global resources are to be provisioned (e.g. \"us-east-1\")."
   type        = string
+}
+
+variable "non_self_admin_users" {
+  default     = []
+  description = "A list containing the usernames of non-admin users that are not allowed to administer their own accounts.  Example: [ \"service-account1\", \"service-account2\", \"service-account3\" ]"
+  type        = list(string)
 }
 
 variable "self_managed_creds_with_mfa_policy_description" {
